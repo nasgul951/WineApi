4599deb266847455f564e8e0833bde331623f78f
Brad Bartlett <box1@bradleyb.us>
2026-01-12 14:22:56 -0500

    Add UserController - extract user methods into new UserService - add middleware to handle exceptions

--- src/WineApi/Service/AuthService.cs
+++ src/WineApi/Service/AuthService.cs
@@ -4,7 +4,7 @@ using WineApi.Data;
 using WineApi.Exceptions;
 using WineApi.Extensions;
 using WineApi.Helpers;
-using WineApi.Model.Auth;
+using WineApi.Model.User;
 
 namespace WineApi.Service;
 
@@ -65,60 +65,6 @@ public class AuthService : IAuthService
         await _db.SaveChangesAsync();
     }
     
-    public async Task<UserDto?> AddUser(AddUpdateUser addUser)
-    {
-        var userExists = await _db.Users.AnyAsync(u => u.Username  == addUser.Username);
-        if (userExists) {
-            throw new ConflictException("Username");
-        }
-
-        var salt = CryptoHelper.GenerateSalt(16);
-        var hashedPassword = CryptoHelper.HashPassword(addUser.Password, salt);
-
-        var user = new User
-        {
-            Username = addUser.Username,
-            IsAdmin = addUser.IsAdmin,
-            Salt = salt,
-            Password = hashedPassword
-        };
-
-        _db.Users.Add(user);
-        await _db.SaveChangesAsync();
-
-        var userDto = await _db.Users
-            .Where(u => u.Username == addUser.Username)
-            .ToUserDto()
-            .FirstOrDefaultAsync();
-        return userDto;
-    }
-
-    public async Task<UserDto?> UpdateUser(int userId, AddUpdateUser updateUser)
-    {
-        var user = await _db.Users.FirstOrDefaultAsync(u => u.Id == userId);
-        if (user == null)
-        {
-            throw new NotFoundException("userId");
-        }
-
-        var usernameExists = await _db.Users.AnyAsync(u => u.Username == updateUser.Username & u.Id != userId);
-        if (usernameExists)
-        {
-            throw new ConflictException("Username");
-        }
-
-        var hashedPassword = CryptoHelper.HashPassword(updateUser.Password, user.Salt);
-        user.Username = updateUser.Username;
-        user.Password = hashedPassword;
-        user.IsAdmin = updateUser.IsAdmin;
-
-        var userDto = await _db.Users
-            .Where(u => u.Id == userId)
-            .ToUserDto()
-            .FirstOrDefaultAsync();
-        return userDto;
-    }
-
     private async Task GenerateUserKey(User user)
     {
         var guid = Guid.NewGuid();
---
+++ src/WineApi/Service/UserService.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WineApi.Data;
+using WineApi.Exceptions;
+using WineApi.Extensions;
+using WineApi.Helpers;
+using WineApi.Model.User;
+
+namespace WineApi.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly WineContext _db;
+        public UserService(WineContext wineContext)
+        {
+            _db = wineContext;
+        }
+
+        public async Task<IQueryable<UserDto>> GetByFilter(UserRequest filter)
+        {
+            var users = _db.Users
+                .IfThenWhere(!string.IsNullOrEmpty(filter.Username), u => EF.Functions.Like(u.Username, $"%{filter.Username}%"))
+                .ToUserDto();
+            
+            return users;
+        }
+
+        public async Task<UserDto> GetById(int userId)
+        {
+            var user = await _db.Users
+                .Where(u => u.Id == userId)
+                .ToUserDto()
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                throw new NotFoundException("userId");
+            }
+            
+            return user;
+        }
+
+        public async Task<UserDto?> AddUser(AddUpdateUser addUser)
+        {
+            var userExists = await _db.Users.AnyAsync(u => u.Username == addUser.Username);
+            if (userExists)
+            {
+                throw new ConflictException("Username");
+            }
+
+            var salt = CryptoHelper.GenerateSalt(16);
+            var hashedPassword = CryptoHelper.HashPassword(addUser.Password, salt);
+
+            var user = new User
+            {
+                Username = addUser.Username,
+                IsAdmin = addUser.IsAdmin,
+                Salt = salt,
+                Password = hashedPassword
+            };
+
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+
+            var userDto = await _db.Users
+                .Where(u => u.Username == addUser.Username)
+                .ToUserDto()
+                .FirstOrDefaultAsync();
+            return userDto;
+        }
+
+        public async Task<UserDto?> UpdateUser(int userId, AddUpdateUser updateUser)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(u => u.Id == userId);
+            if (user == null)
+            {
+                throw new NotFoundException("userId");
+            }
+
+            var usernameExists = await _db.Users.AnyAsync(u => u.Username == updateUser.Username & u.Id != userId);
+            if (usernameExists)
+            {
+                throw new ConflictException("Username");
+            }
+
+            var hashedPassword = CryptoHelper.HashPassword(updateUser.Password, user.Salt);
+            user.Username = updateUser.Username;
+            user.Password = hashedPassword;
+            user.IsAdmin = updateUser.IsAdmin;
+
+            var userDto = await _db.Users
+                .Where(u => u.Id == userId)
+                .ToUserDto()
+                .FirstOrDefaultAsync();
+            return userDto;
+        }
+
+    }
+}
